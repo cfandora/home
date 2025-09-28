1f6809c190b99c56e0f37f2a960df5bc584c1a08
cfandora <cfandora@gmail.com>
2025-09-29 05:23:40 +0800

    base info

--- src/components/MoreContent.vue
+++ src/components/MoreContent.vue
@@ -1,5 +1,13 @@
 <template>
-  <div class="more-content">您可在此编写任意内容</div>
+  <div class="more-content">I'm a bear who came out less than a year ago, or a panda, since I’m Chinese, haha.
+
+I love music — listening and singing. Films are another passion, which I often enjoy alone at home or in a theater. I'm also a gamer, especially for Nintendo. Super Mario is my all-time favorite, the only series I aim to 100% complete.
+
+I used to love playing table tennis with friends, but growing up brought challenges. I became more withdrawn, even struggling to accept myself. Coming out marked a turning point. Since then, I've been working to reclaim my cheerful and outgoing self, embracing the quirks and passions that make me who I am.
+
+My biggest dream right now? To attend Bear Week in P-Town or Lazy Bear Week in Guerneville.
+
+Bears, wait for me — I’m on my way!</div>
 </template>
 
 <style lang="scss" scoped>
